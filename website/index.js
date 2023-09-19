1a9f772a31ced815a8dd371f7250f45f83e85ce2
Gio <gio@chzbrgrops.io>
2023-09-18 22:41:16 -0500

    added infra for lambda

--- website/index.js
+++ website/index.js
@@ -2,7 +2,7 @@
 
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://3mqwq7yxh2crc6lxipp3nd2egm0rqhei.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://db5cvrxxm2lfvgdmmigyvp3nla0lbfkc.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
 }
